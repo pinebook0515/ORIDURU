bb03a0d82ae933dd3b020214c8553c943a26d027
松本拓夢 <matsumototakumu@matsumototakumus-MacBook-Air.local>
2022-01-31 18:00:55 +0900

    crossOriginに変更

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ const Document = () => {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yakuhanjp@3.4.1/dist/css/yakuhanjp.min.css" />
         <link rel="stylesheet" href="https://use.typekit.net/ust7bhs.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&family=Noto+Sans+JP:wght@400;500&display=swap" rel="stylesheet" />
       </Head>
       <body className="w-full bg-black">
